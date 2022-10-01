1a9b15a4960dcc0fb6b2d1785274fab07ba8be7b
Icarus <icaros1105@gmail.com>
2022-09-30 23:27:48 -0300

    Add Javascript logic

---
+++ script.js
@@ -0,0 +1,44 @@
+let button = document.querySelector(".calc");
+button.addEventListener("click", Calculate);
+
+function Calculate() {
+    let height = document.getElementById("h-value").value;
+    let weight = document.getElementById("w-value").value;
+
+    let result = (weight / (height * height * 0.0001));
+    let ideal = 24.9 * (height * height * 0.0001);
+
+
+    if (!+result) {
+        document.querySelector(".bmi-result").textContent = "";
+        document.querySelector(".condition-result").textContent = "";
+        alert("Erro! Digite Apenas numéricos!");
+    } else {
+        if (result < 18.5) {
+            document.querySelector(".bmi-result").textContent = result;
+            document.querySelector(".condition-result").textContent = "Magreza";
+            document.querySelector(".w-ideal").textContent = 18.5 * (height * height * 0.0001);
+        } else if (result >= 18.5 && result < 25) {
+            document.querySelector(".bmi-result").textContent = result;
+            document.querySelector(".condition-result").textContent = "Normal";
+            document.querySelector(".w-ideal").textContent = "";
+        } else if (result >= 25 && result < 30) {
+            document.querySelector(".bmi-result").textContent = result;
+            document.querySelector(".condition-result").textContent = "Sobrepeso";
+            document.querySelector(".w-ideal").textContent = ideal;
+        } else if (result >= 30 && result < 35) {
+            document.querySelector(".bmi-result").textContent = result;
+            document.querySelector(".condition-result").textContent = "Obesidade grau I";
+            document.querySelector(".w-ideal").textContent = ideal;
+        } else if (result >= 35 && result < 40) {
+            document.querySelector(".bmi-result").textContent = result;
+            document.querySelector(".condition-result").textContent = "Obesidade grau II";
+            document.querySelector(".w-ideal").textContent = ideal;
+        } else {
+            document.querySelector(".bmi-result").textContent = result;
+            document.querySelector(".condition-result").textContent = "Obesidade grau III";
+            document.querySelector(".w-ideal").textContent = ideal;
+        }
+    }
+
+}
